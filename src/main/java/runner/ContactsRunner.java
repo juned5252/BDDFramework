bbb074bafece99271fcf8078b82ff6ca93b43650
Juned Alam <juned5252@gmail.com>
2018-11-03 19:18:23 -0400

    added contacts feature file with DDT

---
+++ src/main/java/runner/ContactsRunner.java
@@ -0,0 +1,19 @@
+
+package runner;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "/Users/junedalam/Documents/FreeCrmBDDFramework/src/main/java/features/contacts.feature",
+        glue = {"stepDefinition"},
+        format = {"pretty","html:test-output"},
+        monochrome = true,
+        dryRun = false,
+        strict = true
+)
+
+public class ContactsRunner {
+}
